edaa7e5cb873ae1538f4404672ee133550bc6fbc
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-09-26 09:59:23 +0000

    list values

---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+-- Script: list_values.sql
+-- Description: This script lists all rows of the 'first_table' table and prints all fields.
+
+-- Select all rows from 'first_table' and print all fields
+SELECT * FROM first_table;
