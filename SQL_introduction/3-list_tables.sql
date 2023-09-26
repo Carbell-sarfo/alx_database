33b4173668e34b8b7221d1c3b44611a1f4426654
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-09-26 09:41:39 +0000

    listing errors

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,27 +1,10 @@
--- Script: create_tables.sql
--- Description: This script deletes and recreates the 'hbtn_test_db_0' database and creates 3 tables if they do not already exist.
-
--- Drop the database if it exists
-DROP DATABASE IF EXISTS hbtn_test_db_0;
-
--- Create the database if it does not exist
-CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
+-- Script: list_tables.sql
+-- Description: This script lists all the tables in the 'hbtn_test_db_0' database and formats the output.
 
 -- Use the 'hbtn_test_db_0' database
 USE hbtn_test_db_0;
 
--- Create the 'holbteron_0' table if it does not exist
-CREATE TABLE IF NOT EXISTS holbteron_0 (
-    id INT
-);
+-- List all tables in 'hbtn_test_db_0'
+SHOW TABLES;
 
--- Create the 'holbteron_1' table if it does not exist
-CREATE TABLE IF NOT EXISTS holbteron_1 (
-    name VARCHAR(256)
-);
-
--- Create the 'holbteron_2' table if it does not exist
-CREATE TABLE IF NOT EXISTS holbteron_2 (
-    id INT,
-    name VARCHAR(256)
-);
+-- Provide a custom column header for the output
