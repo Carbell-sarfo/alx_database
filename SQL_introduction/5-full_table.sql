385fdf05f1db1bce526e4f363e2f1766017cdb98
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-09-26 09:53:49 +0000

    request table

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- Script: full_table_description.sql
+-- Description: This script prints the full description of the 'first_table' table.
+
+-- Show the create table statement for 'first_table'
+SHOW CREATE TABLE first_table;
