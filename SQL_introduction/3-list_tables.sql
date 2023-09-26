cb19b50380ba53e6d3502e2412b6c23ebc1a51fa
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-09-26 09:39:45 +0000

    list tables

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,9 +1,27 @@
--- Script: list_tables.sql
--- Description: This script lists all the tables in a specified database.
--- Usage: cat list_tables.sql | mysql -h localhost -u root -p <database_name>
+-- Script: create_tables.sql
+-- Description: This script deletes and recreates the 'hbtn_test_db_0' database and creates 3 tables if they do not already exist.
 
-USE INFORMATION_SCHEMA;
+-- Drop the database if it exists
+DROP DATABASE IF EXISTS hbtn_test_db_0;
 
-SELECT table_name
-FROM tables
-WHERE table_schema = DATABASE();
+-- Create the database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
+
+-- Use the 'hbtn_test_db_0' database
+USE hbtn_test_db_0;
+
+-- Create the 'holbteron_0' table if it does not exist
+CREATE TABLE IF NOT EXISTS holbteron_0 (
+    id INT
+);
+
+-- Create the 'holbteron_1' table if it does not exist
+CREATE TABLE IF NOT EXISTS holbteron_1 (
+    name VARCHAR(256)
+);
+
+-- Create the 'holbteron_2' table if it does not exist
+CREATE TABLE IF NOT EXISTS holbteron_2 (
+    id INT,
+    name VARCHAR(256)
+);
