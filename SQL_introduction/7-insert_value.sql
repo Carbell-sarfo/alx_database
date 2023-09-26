c0aa9bb66ef4a5f23af7e9a16b21e497920a4d83
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-09-26 10:00:47 +0000

    insert value

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+-- Script: insert_value.sql
+-- Description: This script inserts a new row into the 'first_table' table.
+
+-- Insert a new row into 'first_table'
+INSERT INTO first_table (id, name) VALUES (89, 'Holberton School');
