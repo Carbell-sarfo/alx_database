74f490e6b0b74fbde2d12b3df653d574c598f609
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-09-26 10:02:19 +0000

    count

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,5 @@
+-- Script: count_89.sql
+-- Description: This script displays the number of records with id = 89 in the 'first_table' table.
+
+-- Select the count of records with id = 89 from 'first_table'
+SELECT COUNT(*) FROM first_table WHERE id = 89;
