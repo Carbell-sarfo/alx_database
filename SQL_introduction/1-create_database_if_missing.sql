ded5f942bb48d035429a96317767c56857be32c8
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-09-26 09:46:33 +0000

    creating database

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,29 +1,5 @@
-#!/bin/bash
+-- Script: create_database_if_missing.sql
+-- Description: This script creates the 'hbtn_0c_0' database if it does not already exist.
 
-# MySQL credentials
-MYSQL_USER="your_mysql_username"
-MYSQL_PASSWORD="your_mysql_password"
-
-# Database name to create
-DATABASE_NAME="hbtn_0c_0"
-
-# Create a connection to MySQL with the specified database name
-mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" <<EOF
--- This script creates the database hbtn_0c_0 if it doesn't exist.
--- If the database already exists, it will connect to it.
-
--- Create the database if it doesn't exist (ignore any errors)
-CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;
-
--- Use the specified database for subsequent queries
-USE $DATABASE_NAME;
-
--- Your SQL queries and commands can go here.
--- For example:
--- CREATE TABLE IF NOT EXISTS my_table (
---     id INT AUTO_INCREMENT PRIMARY KEY,
---     name VARCHAR(255)
--- );
-
--- Remember to end your script with 'EOF'.
-EOF
+-- Create the 'hbtn_0c_0' database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
