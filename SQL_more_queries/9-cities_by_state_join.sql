dfdb960d499d73089a9e524de1502cd85b7b0ffa
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-09-29 11:31:28 +0000

    problem with 9 again

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,13 +1,11 @@
 -- Script: list_cities_with_states.sql
--- Description: This script lists all cities with their corresponding state names from the 'hbtn_0d_usa' database.
+-- Description: This script lists all cities in the 'hbtn_0d_usa' database with their IDs, names, and corresponding state names.
 
 -- Use the specified database (e.g., hbtn_0d_usa)
 USE hbtn_0d_usa;
 
--- List cities with state names using subqueries
-SELECT
-    cities.id,
-    cities.name,
-    (SELECT name FROM states WHERE id = cities.state_id) AS state_name
+-- List cities with their IDs, names, and corresponding state names
+SELECT cities.id, cities.name, states.name AS state_name
 FROM cities
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id;
