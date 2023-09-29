1fdd3ff54d49e264efa4db0a1781b98760a0ba89
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-09-29 10:51:02 +0000

    8 again

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,11 +1,16 @@
--- Script: cities_of_california_subquery.sql
+-- Script: cities_of_california_no_join.sql
 -- Description: This script lists all the cities of California from the 'hbtn_0d_usa' database without using the JOIN keyword.
 
 -- Use the specified database (e.g., hbtn_0d_usa)
 USE hbtn_0d_usa;
 
--- List the cities of California using subqueries
+-- List the cities of California using a correlated subquery
 SELECT cities.id, cities.name
 FROM cities
-WHERE cities.state_id = (SELECT id FROM states WHERE name = 'California')
+WHERE EXISTS (
+    SELECT 1
+    FROM states
+    WHERE states.name = 'California'
+    AND states.id = cities.state_id
+)
 ORDER BY cities.id;
