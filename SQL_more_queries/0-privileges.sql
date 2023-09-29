c7e6ef7f07ac4e0a309618f952a0ff77c495ff90
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-09-29 09:20:23 +0000

    0 privileges updated

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -2,19 +2,15 @@
 -- Description: This script lists privileges for MySQL users 'user_0d_1' and 'user_0d_2' on localhost.
 
 -- Check and display privileges for 'user_0d_1'
-SELECT IF(
-    (SELECT COUNT(*) FROM mysql.user WHERE user = 'user_0d_1' AND host = 'localhost') > 0,
-    "Grants for user_0d_1@localhost",
-    "There is no such grant defined for user 'user_0d_1' on host 'localhost'"
-) AS Grant_Description;
-
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+IF EXISTS (SELECT 1 FROM mysql.user WHERE user = 'user_0d_1' AND host = 'localhost') THEN
+    SHOW GRANTS FOR 'user_0d_1'@'localhost';
+ELSE
+    SELECT "There is no such grant defined for user 'user_0d_1' on host 'localhost'" AS Grant_Description;
+END IF;
 
 -- Check and display privileges for 'user_0d_2'
-SELECT IF(
-    (SELECT COUNT(*) FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost') > 0,
-    "Grants for user_0d_2@localhost",
-    "There is no such grant defined for user 'user_0d_2' on host 'localhost'"
-) AS Grant_Description;
-
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+IF EXISTS (SELECT 1 FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost') THEN
+    SHOW GRANTS FOR 'user_0d_2'@'localhost';
+ELSE
+    SELECT "There is no such grant defined for user 'user_0d_2' on host 'localhost'" AS Grant_Description;
+END IF;
