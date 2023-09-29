30a5dd489a745842ae4b09a65e435b00536b7aee
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-09-29 10:01:50 +0000

    0 privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,20 +1,11 @@
--- Script: privileges.sql
--- Description: This script lists privileges for MySQL users 'user_0d_1' and 'user_0d_2' on localhost.
+-- Script: create_unique_id_table.sql
+-- Description: This script creates the 'unique_id' table with the specified schema.
 
--- Check and display privileges for 'user_0d_1'
-SELECT IF(
-    (SELECT COUNT(*) FROM mysql.user WHERE user = 'user_0d_1' AND host = 'localhost') > 0,
-    "Grants for user_0d_1@localhost",
-    "There is no such grant defined for user 'user_0d_1' on host 'localhost'"
-) AS Grant_Description;
+-- Create the 'unique_id' table if it doesn't exist
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
 
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- Check and display privileges for 'user_0d_2'
-SELECT IF(
-    (SELECT COUNT(*) FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost') > 0,
-    "Grants for user_0d_2@localhost",
-    "There is no such grant defined for user 'user_0d_2' on host 'localhost'"
-) AS Grant_Description;
-
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- Use the specified database (e.g., hbtn_0d_2)
+USE hbtn_0d_2;
