cae29c551a7d28af5922d09a316b82ae5af61cc0
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2023-09-29 10:34:39 +0000

    adding 9 cities

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,11 @@
+-- Script: list_all_cities.sql
+-- Description: This script lists all cities from the 'hbtn_0d_usa' database along with their respective state names.
+
+-- Use the specified database (e.g., hbtn_0d_usa)
+USE hbtn_0d_usa;
+
+-- List all cities with their state names
+SELECT cities.id, cities.name, states.name
+FROM cities
+LEFT JOIN states ON cities.state_id = states.id
+ORDER BY cities.id;
